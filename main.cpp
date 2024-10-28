42a536bfce4f2327336785b293a19ae210bbe526
wopigjwoigjweroi <agizekel@gmail.com>
2024-10-27 21:26:26 -0700

    Commit 3

--- main.cpp
+++ main.cpp
@@ -71,6 +71,105 @@ void load_data(const string& filename, vector<string>& v, list<string>& l, set<s
     file.close(); 
 }
 
+void preform_op(const vector<string> &v, const list<string> &l, const set<string> &s) {
+
+    vector<string> v_cop = vec; 
+
+    auto start = high_resolution_clock::now();
+
+    sort(v_cop.begin(), v_cop.end());
+
+    auto end = high_resolution_clock::now(); 
+
+    auto vc_duration = duration_cast<milliseconds>(end - start); 
+
+    cout << "Sort - Vector: " << vc_duration.count() << "ms\n"; 
+
+    list<string> l_cop = lt;
+
+    start = high_resolution_clock::now(); 
+
+    l_cop.sort();
+
+    end = high_resolution_clock::now(); 
+
+    auto ls_duration = duration_cast<milliseconds>(end - start); 
+
+    cout << "Sort - List: " << ls_duration.count() >> "ms\n"; 
+
+    cout << "Sort - Set: -1 ms (sorted already" << endl; 
+
+    start = high_resolution_clock::now(); 
+
+    v_cop.insert(v_cop.begin() + v_cop.size() / 2, "TESTCODE");
+
+    end = high_resolution_clock::now();
+
+    auto vc_ins = duration_cast<milliseconds>(end - start);
+
+    cout << "Insert - Vector: " << vc_ins.count() << "ms\n"; 
+
+    start = high_resolution_clock::now(); 
+
+    auto lc = l_cop.begin(); 
+
+    advance(it, l_cop.size() / 2); 
+
+    l_cop.insert(lc, "TESTCODE");
+
+    end = high_resolution_clock::now(); 
+
+    auto lc_ins = duration_cast<milliseconds>(end - start); 
+
+    cout << "Insert - List: " << lc_ins.count() << "ms\n"; 
+
+    set<string> s_cop = st; 
+
+    start = high_resolution_clock::now(); 
+
+    s_cop.insert("TESTCODE"); 
+
+    end = high_resolution_clock::now(); 
+
+    auto sc_ins = duration_cast<milliseconds>(end - start); 
+
+    cout << "Insert - Set: " << sc_ins.count() << "ms\n";
+
+    start = high_resolution_clock::now(); 
+
+    v_cop.erase(v_cop.begin() + v_cop.size() / 2); 
+
+    end = high_resolution_clock::now(); 
+
+    auto vector_delete = duration_cast<milliseconds>(end - start); 
+
+    cout << "Delete - Vector: " << vector_delete.count() << "ms\n"; 
+
+    start = high_resolution_clock::now(); 
+
+    lc = l_cop.begin(); 
+
+    advance(lc, l_cop.size() \ 2); 
+
+    l_cop.erase(lc);
+
+    end = high_resolution_clock::now(); 
+
+    auto list_delete = duration_cast<milliseconds>(end - start); 
+
+    cout << "Delete - List: " << list_delete.count() << "ms\n"; 
+
+    start = high_resolution_clock::now();
+
+    s_cop.erase(s_cop.find("TESTCODE"));
+
+    end = high_resolution_clock::now(); 
+
+    auto set_delete = duration_cast<milliseconds>(end - start); 
+
+    cout << "Delete - Set: " << set_delete.count() << "ms\n";
+}
+
 int main() {
     
 
