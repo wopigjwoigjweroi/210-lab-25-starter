76fdeb150b3e0a8fbd67273e49e4f4b514f143f4
wopigjwoigjweroi <agizekel@gmail.com>
2024-10-27 20:59:38 -0700

    Commit 2

--- main.cpp
+++ main.cpp
@@ -15,7 +15,61 @@ const string FILE = "codes.txt";
 void load_data(const string& filename, vector<string>& v, list<string>& l, set<string> s); 
 void preform_op(const vector<string> &v, const list<string> &l, const set<string> &s); 
 
+void load_data(const string& filename, vector<string>& v, list<string>& l, set<string> s) {
 
+    fstream file("codes.txt");
+
+    string s; 
+
+    auto start = high_resolution_clock::now(); 
+
+    while (getline(file, s)) {
+
+        v.push_back(s); 
+    }
+
+    auto end = high_resolution_clock::now(); 
+
+    auto v_duration = duration_cast<milliseconds>(end - start);
+
+    cout << "Read - Vector: " << v_duration.count() << "ms\n"; 
+
+    file.clear();
+
+    file.seekg(0); 
+
+    start = high_resolution_clock::now(); 
+
+    while (getline(file, s)) {
+
+        l.push_back(s);
+    }
+
+    end = high_resolution_clock::now(); 
+
+    auto l_duration = duration_cast<milliseconds>(end - start);
+
+    cout << "Read - List: " << l_duration.count() << "ms\n"; 
+
+    file.clear(); 
+
+    file.seekg(0); 
+
+    start = high_resolution_clock::now(); 
+
+    while (getline(file, s)) {
+
+        s.insert(s);
+    }
+
+    end = high_resolution_clock::now(); 
+
+    auto s_duration = duration_cast<milliseconds>(end - start); 
+
+    cout << "Read - Set: " << s_duration.count() << "ms\n"; 
+
+    file.close(); 
+}
 
 int main() {
     
